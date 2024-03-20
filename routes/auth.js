1ee89c1c7f2d67a47dda30e3db4d2ea5d8350eaf
IntrvrtSmtry <116476924+Semetery29@users.noreply.github.com>
2024-03-21 01:45:30 +0600

    facebook login strategy configured

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ var express = require("express");
 var passport = require("passport");
 var LocalStrategy = require("passport-local");
 var GoogleStrategy = require("passport-google-oidc");
+var FacebookStrategy = require("passport-facebook");
 var crypto = require("crypto");
 var db = require("../db");
 
@@ -117,11 +118,74 @@ passport.use(
     }
   )
 );
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env["FACEBOOK_CLIENT_ID"],
+      clientSecret: process.env["FACEBOOK_CLIENT_SECRET"],
+      callbackURL: "/oauth2/redirect/facebook",
+      state: true,
+    },
+    function verify(accessToken, refreshToken, profile, cb) {
+      db.get(
+        "SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
+        ["https://www.facebook.com", profile.id],
+        function (err, row) {
+          if (err) {
+            return cb(err);
+          }
+          if (!row) {
+            db.run(
+              "INSERT INTO users (name) VALUES (?)",
+              [profile.displayName],
+              function (err) {
+                if (err) {
+                  return cb(err);
+                }
+
+                var id = this.lastID;
+                db.run(
+                  "INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
+                  [id, "https:www.facebook.com", profile.id],
+                  function (err) {
+                    if (err) {
+                      return cb(err);
+                    }
+                    var user = {
+                      id: id,
+                      name: profile.displayName,
+                    };
+                    return cb(null, user);
+                  }
+                );
+              }
+            );
+          } else {
+            db.get(
+              "SELECT * FROM users WHERE id = ?",
+              [row.user_id],
+              function (err, row) {
+                if (err) {
+                  return cb(err);
+                }
+                if (!row) {
+                  return cb(null, false);
+                }
+                return cb(null, row);
+              }
+            );
+          }
+        }
+      );
+    }
+  )
+);
 
 router.get("/login", function (req, res, next) {
   res.render("login");
 });
 router.get("/login/federated/google", passport.authenticate("google"));
+router.get("/login/federated/facebook", passport.authenticate("facebook"));
 router.get(
   "/oauth2/redirect/google",
   passport.authenticate("google", {
